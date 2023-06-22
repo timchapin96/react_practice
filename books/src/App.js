7be15cbe481c9fc99944b404fb65e98f419b5f8c
Tim Chapin <chapin.timothys@gmail.com>
2023-06-22 19:26:45 +0900

    ContextPractice

--- books/src/App.js
+++ books/src/App.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 function App() {
   const [books, setBooks] = useState([]);
 
-  const fetchBooks = async() => {
+  const fetchBooks = async () => {
     const response = await axios.get("http://localhost:3001/books");
     setBooks(response.data);
   }
@@ -15,17 +15,25 @@ function App() {
     fetchBooks();
   }, []);
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async(id) => {
+
+    await axios.delete(`http://localhost:3001/books/${id}`)
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
       return book;
