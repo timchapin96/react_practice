cc1be1444d9898e663849dea86493a89f74e8c5b
Tim Chapin <chapin.timothys@gmail.com>
2023-12-28 13:37:21 -0500

    We kinda did it

--- todo-list/src/app/todo/page.js
+++ todo-list/src/app/todo/page.js
@@ -1,7 +1,55 @@
+'use client'
+
+import { useState } from "react";
+
+
 export default function Page() {
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState('');
+  const [checkedTasks, setCheckedTasks] = useState({});
+
+  function handleChange(event) {
+    setTask(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setList(
+      [
+        ...list,
+        task
+      ]);
+  };
+
+  const handleCheck = (task) => {
+    setCheckedTasks(checkedTasks => ({
+      ...checkedTasks,
+      [task] : !checkedTasks[task]
+    }));
+  }
+
+
+
   return (
     <div>
-      <h1>ToDo List page</h1>
+      <h1 className="text-center">ToDo List page</h1>
+      <div>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center m-3 space-y-5">
+          <label htmlFor="task">Task</label>
+          <input type="text" id="task" name="task" className="text-black" onChange={handleChange}></input>
+          <input type="submit" value="Submit" className="bg-green-800 text-white p-3"></input>
+        </form>
+      </div>
+      <div>
+        <ul>
+          {list.map(task => (
+            <form key={task}>
+              <input type="checkbox" id="taskCheck" name="task" onChange= {() => handleCheck(task)} />
+              <label htmlFor="taskCheck" className={`${checkedTasks[task] ? 'text-gray-500' : ''}`}>{task}</label>
+            </form>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
