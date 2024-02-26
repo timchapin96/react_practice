c893d52a9b3dd45d13bc4c35df89bd89b4c4a584
Tim Chapin <chapin.timothys@gmail.com>
2024-02-26 17:01:54 +0900

    stat padding

--- vite_comps/src/pages/TablePage.jsx
+++ vite_comps/src/pages/TablePage.jsx
@@ -1,4 +1,5 @@
-import Table from "../components/Table"
+// import Table from "../components/Table"
+import SortableTable from "../components/SortableTable";
 
 
 function TablePage() {
@@ -13,7 +14,8 @@ function TablePage() {
   const config = [
     {
       label: "Name",
-      render: (fruit) => fruit.name
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name
     },
     {
       label: "Color",
@@ -21,7 +23,8 @@ function TablePage() {
     },
     {
       label: "Score",
-      render: (fruit) => fruit.score
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score
     }
 
   ]
@@ -31,7 +34,7 @@ function TablePage() {
   }
   return (
     <div>
-      <Table data={data} config = {config} keyFn = {keyFn} />
+      <SortableTable data={data} config = {config} keyFn = {keyFn} />
     </div>
 
   )
