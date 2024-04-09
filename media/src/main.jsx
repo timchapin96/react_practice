1e9fa5cc10a86f25d8ba3a05fb4f9c8976f58e97
Tim Chapin <chapin.timothys@gmail.com>
2024-04-09 15:56:18 +0900

    Enviorment setup complete

--- media/src/main.jsx
+++ media/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+
+import { Provider } from 'react-redux'
+import { store } from './store'
 import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import App from './App'
 import './assets/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
