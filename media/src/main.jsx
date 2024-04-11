0965c89b73e3e5d864c54039f281f7e5a62738f2
Tim Chapin <chapin.timothys@gmail.com>
2024-04-11 21:37:21 +0900

    More comments

--- media/src/main.jsx
+++ media/src/main.jsx
@@ -6,6 +6,7 @@ import App from './App'
 import './assets/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //Set provider with prop of store to give access to Redux store
   <Provider store={store}>
     <App />
   </Provider>
