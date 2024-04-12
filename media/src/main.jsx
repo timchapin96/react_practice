58205639f459b2c2e928f0957737e621ea8f3590
Tim Chapin <chapin.timothys@gmail.com>
2024-04-12 21:43:28 +0900

    Documentation

--- media/src/main.jsx
+++ media/src/main.jsx
@@ -8,6 +8,8 @@ import './assets/index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   //Set provider with prop of store to give access to Redux store
   <Provider store={store}>
+    {/* Today sucked at work. I had to grade tests for 7 hours. But the streak must live on */}
+    {/* This is where I put the app in the provider */}
     <App />
   </Provider>
 )
