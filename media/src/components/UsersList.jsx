e0d5ab23084bd5da4a60add2f84ac3b092c748d9
Tim Chapin <chapin.timothys@gmail.com>
2024-05-09 16:07:14 +0900

    Added loading animation

--- media/src/components/UsersList.jsx
+++ media/src/components/UsersList.jsx
@@ -1,22 +1,47 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchUsers } from '../store'
+import { fetchUsers, addUser } from '../store'
+import Button from './Button'
+import Skeleton from './loaderSkeleton'
 
-export default function UsersList() {
+export default function UsersList () {
   const dispatch = useDispatch()
-  const [isLoading, data, error] = useSelector ((state) => {
+  const { isLoading, data, error } = useSelector(state => {
     return state.users
   })
   useEffect(() => {
     dispatch(fetchUsers())
   }, [dispatch])
 
+  const handleUserAdd = () => {
+    dispatch(addUser())
+  }
+
   if (isLoading) {
-    return <div>Loading...</div>
+    return <Skeleton times={6} className='h-10 w-full' />
   }
   if (error) {
     return <div>Error fetching data...</div>
   }
 
-  return <div>{data.length}</div>
+  const renderedUsers = data.map(user => {
+    return (
+      <div key={user.id} className='mb-2 border rounded'>
+        <div className='flex p-2 justify-between items-center cursor-pointer'>
+          {user.name}
+        </div>
+      </div>
+    )
+  })
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>
+          + Add User
+        </Button>
+      </div>
+      {renderedUsers}
+    </div>
+  )
 }
--- media/src/store/thunks/fetchUsers.jsx
+++ media/src/store/thunks/fetchUsers.jsx
@@ -4,7 +4,16 @@ import axios from 'axios'
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users')
 
+  //Dev ONLY!
+  await pause(1000)
+
   return response.data
 })
 
+//Dev Only!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration)
+  })
+}
 export { fetchUsers }
