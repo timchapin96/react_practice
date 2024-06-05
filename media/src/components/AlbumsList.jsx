0e1f822977d3267f9532ae073fca3feb59b1f751
Tim Chapin <chapin.timothys@gmail.com>
2024-06-05 15:48:39 +0900

    Made redux tool api with endpoint

--- media/src/components/AlbumsList.jsx
+++ media/src/components/AlbumsList.jsx
@@ -1,5 +1,9 @@
-export default function AlbumsList({user}) {
-  return <div>
-    Albums for {user.name}
-  </div>
+import { useFetchAlbumsQuery } from '../store'
+
+export default function AlbumsList ({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user)
+
+  console.log(data, error, isLoading);
+
+  return <div>Albums for {user.name}</div>
 }
--- media/src/store/apis/ablumsApi.jsx
+++ media/src/store/apis/ablumsApi.jsx
@@ -1,5 +1,26 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const albumsApi = createApi({
-  reducerPath: 'ablums',
+  reducerPath: 'albums',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3005'
+  }),
+  endpoints(builder) {
+    return {
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: '/albums',
+            params : {
+              userId: user.id
+            },
+            method: 'GET',
+          }
+        }
+      })
+    }
+  }
 })
+
+export const { useFetchAlbumsQuery } = albumsApi
+export { albumsApi }
